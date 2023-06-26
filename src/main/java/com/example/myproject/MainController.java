f1ecc03c04a9841afdf1edf26f695339ce180d9d
Omkar Bhattarai <omkarbhattarai71@gmail.com>
2023-06-26 09:11:11 +0530

    second comit
    Till product orders

--- src/main/java/com/example/myproject/MainController.java
+++ src/main/java/com/example/myproject/MainController.java
@@ -1,13 +1,13 @@
 package com.example.myproject;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -15,6 +15,12 @@ public class MainController{
 
     @Autowired
     CredentialRepository credentialRepository;
+
+    @Autowired
+    UserdetailRepository userdetailRepository;
+
+    @Autowired
+    UsertypelinkRepository usertypelinkRepository;
 
     @GetMapping("/")
     public String getLangingPage(){
@@ -30,24 +36,71 @@ public class MainController{
         return  "dashboard";
 
     }
+    @PostMapping("/userdetails")
+    public String userDetails(@RequestParam("username") String username,
+                              @RequestParam("fname") String fname,
+                              @RequestParam("lname") String lname,
+                              @RequestParam("email") String email,
+                              @RequestParam("phone") String phone, HttpSession session)
+    {
+        Userdetail udetail = new Userdetail();
+        udetail.setUsername(username);
+        udetail.setFname(fname);
+        udetail.setEmail(email);
+        udetail.setPhone(phone);
+        udetail.setLname(lname);
+        userdetailRepository.save(udetail);
+        return "usertypelink";
+    }
+
+    @PostMapping("/usertypelinks")
+    public String usertypeLinks(@RequestParam("id") String id,
+                                @RequestParam("username") String username,
+                                @RequestParam("type") String type, HttpSession session){
+        Usertypelink utypelink = new Usertypelink();
+        utypelink.setId(id);
+        utypelink.setUsername(username);
+        utypelink.setType(type);
+        usertypelinkRepository.save(utypelink);
+        return "usertypelink";
+    }
     @PostMapping("/login")
-    public String login(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session){
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session, Model model){    // @RequestParam("type") String type
         Optional<Credential> matchedCredential = credentialRepository.findById(username);
         if(matchedCredential.isPresent()){
             if(matchedCredential.get().getPassword().equals(password)){
                 session.setAttribute("username", username);
-                return "dashboard";
+                Optional<Userdetail> userdetail = userdetailRepository.findById(username);
+                List<Usertypelink> usertypelinks = usertypelinkRepository.findAll();
+                Optional<Usertypelink> usertypelink = usertypelinks.stream().filter(usertypelink1 -> usertypelink1.getUsername().equals(username)).findAny();
+
+                if(userdetail.isPresent()){
+                    model.addAttribute("usedetail",userdetail);
+
+                    if(usertypelink.get().getType().equals("BUYER")){
+                        return "buyerdashboard";
+
+                    }
+                    else if(usertypelink.get().getType().equals("SELLER")){
+                        return "sellerdashboard";
+                    }
+                    else{
+                        return "interimdashboard";
+                    }
+                }
+//                userdetail.ifPresent(value -> model.addAttribute("userdetail", value));
+
+                return "interimdashboard";
             }
             else{
                 return "landingpage";
             }
-
         }
         else {
-            return "dashboard";
+            return "landingpage";
         }
-
     }
+
     @GetMapping("/save")
     public String saveCredential(){
         Credential cr = new Credential();
---
+++ src/main/java/com/example/myproject/UsertypelinkRepository.java
@@ -0,0 +1,6 @@
+package com.example.myproject;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsertypelinkRepository extends JpaRepository<Usertypelink, String> {
+}
